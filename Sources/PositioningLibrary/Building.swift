6adfd80fe2d27eee59fe344496f8b21d5a86f90e
tirannosario <glt.rosario@gmail.com>
2022-08-20 21:54:50 +0200

    update

--- Sources/PositioningLibrary/Building.swift
+++ Sources/PositioningLibrary/Building.swift
@@ -1,25 +1,19 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Rosario Galioto on 27/07/22.
 //
-#if os(iOS)
 
 import Foundation
 
-public struct Building {
+public class Building {
+    public var id: String
     public var name: String
-    public var floors: [Floor]
     
-    public init (_ name: String) {
+    public init (id: String, name: String) {
+        self.id = id
         self.name = name
-        self.floors = []
-    }
-    
-    public mutating func addFloor(_ floor: Floor) {
-        self.floors.append(floor)
     }
 }
 
-#endif
